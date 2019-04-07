7c3465068a9b447d934c8edf9c453167e76461eb
Annalee Flower Horne <annalee@users.noreply.github.com>
2019-04-07 16:54:13 -0400

    preliminary tests for submission pages

---
+++ submissions/tests/test_views.py
@@ -0,0 +1,17 @@
+from django.test import Client, SimpleTestCase
+from django.urls import reverse
+
+class Test_Pages(SimpleTestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_panel(self):
+        response = self.client.get(reverse('panel'))
+
+        self.assertContains(response, "I'm a panel submission!")
+
+    def test_panelist(self):
+        response = self.client.get(reverse('panelist'))
+
+        self.assertContains(response, "I'm a panelist submission!")
