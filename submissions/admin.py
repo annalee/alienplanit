d2926ca9f4ff9c61e50c7da6f8bfb10998650ad7
Annalee Flower Horne <annalee@users.noreply.github.com>
2019-04-07 15:53:05 -0400

    fixing indentation because fml

--- submissions/admin.py
+++ submissions/admin.py
@@ -4,20 +4,24 @@ from .models import Panelist, Panel
 
 @admin.register(Panelist)
 class PanelistAdmin(admin.ModelAdmin):
-    list_display 	= ["badge_name",
-    				   "email",
-    				   "conference"]
-    search_fields 	= ["badge_name",
-    				   "email"]
+    list_display = [
+        "badge_name",
+        "email",
+        "conference"]
+    search_fields = [
+        "badge_name",
+        "email"]
     readonly_fields = ["conference"]
     list_filter = ["conference"]
 
 
 @admin.register(Panel)
 class PanelAdmin(admin.ModelAdmin):
-	list_display  = ["title",
-    				 "submitter_email",
-    				 "conference"]
-    search_fields = ["title",
-    				 "submitter_email"]
-    list_filter   = ["conference"]
+    list_display = [
+        "title",
+        "submitter_email",
+        "conference"]
+    search_fields = [
+        "title",
+        "submitter_email"]
+    list_filter = ["conference"]
