7b339cd69bccb2da9196ab5bcec67eb31b1632a8
Annalee Flower Horne <annalee@users.noreply.github.com>
2019-04-07 21:26:13 -0400

    adding a trailing / to urls to mimic admin urls

--- submissions/urls.py
+++ submissions/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('panel', views.panel, name='panel'),
-	path('panelist', views.panelist, name='panelist'),
+    path('panel/', views.panel, name='panel'),
+    path('panelist/', views.panelist, name='panelist'),
 ]
