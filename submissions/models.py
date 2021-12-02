fc8a7fb64167d459bef8f408a379846473029a99
Annalee Flower Horne <annalee@users.noreply.github.com>
2021-12-01 23:11:13 -0500

    removing hard-coding from submissions forms; now handles multiple conferences and form open/closed controlled from admin.

--- submissions/models.py
+++ submissions/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 
+class Conference(models.Model):
+    slug = models.SlugField(max_length=50, unique=True)
+    name = models.CharField(max_length=280)
+    panelist_form_open = models.BooleanField(default=False)
+    panel_form_open = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.slug
+
+    # Setting a default con for data migration purposes. Way more work than this
+    # warranted when I could have just set a temp default in the migration
+    # dialogue, but this is more future-proof. Have an easter egg: 1843 is the
+    # year of Ada Lovelace's first program.
+    @staticmethod
+    def get_default_con():
+        conference, created = Conference.objects.get_or_create(
+            slug="AlienPlanitCon1843")
+        if created:
+            conference.name = "AlienPlanit Con 1843"
+            conference.save()
+        return conference.pk
+
+
 class Panelist(models.Model):
     # this collects info we can use to contact interested panelists about
     # signing up for panels
@@ -16,7 +39,12 @@ class Panelist(models.Model):
 
     email = models.CharField(max_length=280)
     name = models.CharField(max_length=280)
-    conference = models.SlugField(max_length=50)
+    conference = models.ForeignKey(
+        Conference,
+        on_delete = models.CASCADE,
+        related_name = 'Panelists',
+        default= Conference.get_default_con()
+   )
     returning = models.BooleanField(default=False)
     bio = models.TextField(
         blank=True,
@@ -48,7 +76,12 @@ class Panel(models.Model):
     ]
 
     title = models.CharField(max_length=280)
-    conference = models.SlugField(max_length=50)
+    conference = models.ForeignKey(
+        Conference,
+        on_delete = models.CASCADE,
+        related_name = 'Panels',
+        default= Conference.get_default_con()
+   )
     submitter_email = models.CharField(max_length=280)
     description = models.TextField(blank=True, null=True)
     notes = models.TextField(blank=True, null=True)
@@ -65,10 +98,17 @@ class Panel(models.Model):
     def get_absolute_url(self):
         return reverse('pending-panel-detail', kwargs={'pk': self.pk})
 
+
 class Textblock(models.Model):
     slug=models.SlugField(max_length=50)
-    conference = models.SlugField(max_length=50)
+    conference = models.ForeignKey(
+        Conference,
+        on_delete = models.CASCADE,
+        related_name = 'Textblocks',
+        default= Conference.get_default_con()
+   )
     title=models.CharField(max_length=280, blank=True, null=True)
     body=models.TextField(blank=True, null=True)
 
-    models.UniqueConstraint(fields=['slug', 'conference'], name='unique_textblock')
+    models.UniqueConstraint(
+        fields=['slug', 'conference'], name='unique_textblock')
